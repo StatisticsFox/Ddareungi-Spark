a7dad097471df0c9744fc7599b0347dd8e391404
함승진 <a01079072289@gmail.com>
2024-08-13 21:34:51 +0900

    [승진]modify_kafka_to_redis_and_redis_config

--- Spark-streaming/redis_config.py
+++ Spark-streaming/redis_config.py
@@ -8,10 +8,10 @@ class RedisConfig:
         self.master_name = master_name
         self.redis_auth = redis_auth
 
-    def get_redis_master(self):
-        sentinel = redis.sentinel.Sentinel(self.sentinel_hosts)
-        master = sentinel.discover_master(self.master_name)
-        return master
+    # def get_redis_master(self):
+    #   sentinel = redis.sentinel.Sentinel(self.sentinel_hosts)
+    #  master = sentinel.discover_master(self.master_name)
+    #   return master
     
     # Redis에 데이터를 쓰는 함수
     def write_to_redis(self, batch_df, batch_id):
